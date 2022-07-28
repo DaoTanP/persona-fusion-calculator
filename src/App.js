2948bcafa692546cd7861fd0cf7b283b441ae7f1
Phat <daotanp@gmail.com>
2022-07-28 15:28:00 +0700

    tanDao

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './css/style.css';
 import PersonaCardContainer from './components/PersonaCardContainer';
+import { Route, HashRouter, Switch } from "react-router-dom";
+import React from 'react';
 
 function App() {
   return (
     <>
-      <h1 style={{ textAlign: 'center' }}>
-        Persona 3 Portable Fusion Calculator
-      </h1>
-      <PersonaCardContainer />
+      <React.StrictMode>
+        <HashRouter>
+          <Switch>
+            <Route exact path="/">
+              <h1 style={{ textAlign: 'center' }}>
+                Persona 3 Portable Fusion Calculator
+              </h1>
+              <PersonaCardContainer />
+            </Route>
+          </Switch>
+        </HashRouter>
+      </React.StrictMode>
     </>
   );
 }
